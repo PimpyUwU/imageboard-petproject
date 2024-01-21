bc2d556118a530e07baaeda47a97333734fc0928
PimpyUwU <pimpy.owo@gmail.com>
2024-01-21 16:11:09 +0200

    Created service and repo for boards, Created ViewModel for main page

---
+++ src/repositories/BoardsRepository.ts
@@ -0,0 +1,12 @@
+import {BoardsListViewModel} from "../../types/models/BoardsListViewModel";
+import {BoardViewModel} from "../../types/models/BoardViewModel";
+
+export const BoardsRepository = {
+    GetAllBoards(): BoardsListViewModel {
+
+    },
+
+    GetBoardByTag(tag: string): BoardViewModel {
+
+    }
+}
---
+++ src/services/BoardsService.ts
@@ -0,0 +1,12 @@
+import {BoardViewModel} from "../../types/models/BoardViewModel";
+import {BoardsListViewModel} from "../../types/models/BoardsListViewModel";
+
+export const BoardsService = {
+    GetAllBoards() : BoardsListViewModel {
+
+    },
+
+    GetBoardByTag(tag : string) : BoardViewModel{
+
+    }
+}
