2c338a9978eef4df80ec4fefd1fd19212f7928c3
PimpyUwU <pimpy.owo@gmail.com>
2024-01-28 22:51:15 +0200

    added error handling for Admin registration

--- src/repositories/AdminRepository.ts
+++ src/repositories/AdminRepository.ts
@@ -1,4 +1,4 @@
-import {PrismaClient} from "@prisma/client"
+import {PrismaClient, Prisma} from "@prisma/client"
 import {UserORMModelIn} from "../../types/models/Admin/Input/ORM/UserORMModelIn";
 import {UserORMModelOut} from "../../types/models/Admin/Output/ORM/UserORMModelOut";
 import bcrypt from "bcrypt";
@@ -7,21 +7,29 @@ const prisma : PrismaClient = new PrismaClient()
 
 
 export const AdminRepository = {
-    async signUp(userData : UserORMModelIn) : Promise<UserORMModelOut | null>  {
+    signUp: async function (userData: UserORMModelIn): Promise<UserORMModelOut | null> {
         //Hashing password
         const salt = await bcrypt.genSalt()
         userData.userPassword = await bcrypt.hash(userData.userPassword, salt)
 
-        return prisma.user.create({
-            data : {
-                nickname : userData.userNickname,
-                email : userData.userEmail,
-                password : userData.userPassword
-            },
-            include : {
-                Deleted_posts : true,
-                Deleted_replies : true
+        try {
+            return prisma.user.create({
+                data: {
+                    nickname: userData.userNickname,
+                    email: userData.userEmail,
+                    password: userData.userPassword
+                },
+                include: {
+                    Deleted_posts: true,
+                    Deleted_replies: true
+                }
+            })
+        } catch (e) {
+            if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002') {
+                throw new Error("Email is already used")
             }
-        })
+
+            return null
+        }
     }
 }
